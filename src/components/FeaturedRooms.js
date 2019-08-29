4d9bbad1e6247ac1b142132a6a2032ccd484bd63
Conrad <theradistone@gmail.com>
2019-08-29 19:48:17 -0400

    dummy data

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import Title from "./Title";
-// import { RoomContext } from "../context";
+import { RoomContext } from "../context";
 import Room from "./Room";
 import Loading from "./Loading";
 export default class FeaturedRooms extends Component {
-  // static contextType = RoomContext;
+  static contextType = RoomContext;
 
   render() {
     let { loading, featuredRooms: rooms } = this.context;
