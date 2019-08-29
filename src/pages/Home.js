e37148904cb7e52fd5d2144b6db03953e0d96cdf
Conrad <theradistone@gmail.com>
2019-08-29 17:27:11 -0400

    finish services error componenet

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,26 @@
 import React from "react";
 import Hero from "../components/Hero";
+import { Link } from "react-router-dom";
+import Banner from "../components/Banner";
+import Services from "../components/Services";
+import FeaturedRooms from "../components/FeaturedRooms";
+const home = () => {
+  return (
+    <>
+      <Hero>
+        <Banner
+          title="luxurious rooms"
+          subtitle="Deluxe Rooms starting at $499"
+        >
+          <Link to="/rooms" className="btn-primary">
+            Our Rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
+      <FeaturedRooms />
+    </>
+  );
+};
 
-export default function Home() {
-  return <Hero className="defaultHero" />;
-}
+export default home;
